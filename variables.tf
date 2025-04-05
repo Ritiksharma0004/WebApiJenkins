a3e2cacec17943e98e781ef72eadea7bb15c2300
Ritiksharma0004 <142799642+Ritiksharma0004@users.noreply.github.com>
2025-04-05 10:17:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -21,7 +21,7 @@ variable "subscription_id" {
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
-  default     = "jenkins-webapp-rg"
+  default     = "jenkins-ritik-rg"
 }
 
 variable "location" {
